5017d6c4335c558289a9629e4a1b67fe5093ec39
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-25 18:31:12 -0500

    proto for getClothes, getSingleClothing, createClothig, updateClothing. update schema and seed with more data

---
+++ db/seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO clothes (
+    name,
+    category, 
+    heat, 
+    cold, 
+    material, 
+    iscomfortable,
+    imageurl
+)
+VALUES (
+        't-shirt',
+        'torso',
+        2,
+        3,
+        'cotton',
+        TRUE,
+        'www.google.com'
+    ),
+    (
+        'jeans',
+        'legs',
+        4,
+        1,
+        'denim',
+        TRUE,
+        'https://m.media-amazon.com/images/I/918vV4ZqEIL._AC_SY550_.jpg'
+    ),
+    ()
+    ;
