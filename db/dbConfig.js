b2027c75573a062d58fa081db0ff91294225e8a9
Nugget-Meister <tremainecamacho@gmail.com>
2023-11-27 02:39:52 -0500

    connect user was using password as user

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,7 +6,7 @@ const connect = {
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
-    user: process.env.PG_PASSWORD,
+    user: process.env.PG_USER,
     password: process.env.PG_PASSWORD
 }
 
